11788f40bcb364c8867179639f16de820591ecfe
szpic <szpicus@interia.pl>
2017-03-26 10:33:27 +0200

    Refactor + new queries

--- bin/app.ts
+++ bin/app.ts
@@ -3,40 +3,62 @@ import * as mongodb from "mongodb";
 import * as routes from "./routes/index";
 import * as db from './db';
 
+import { Item } from './shared/Item';
+import { Login } from './shared/Login';
+import { User } from './shared/User';
+
 var app = express();
+
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
 
 app.get('/', routes.index);
 
-app.get('/user/:userid', (req, res) => {
-    console.log('getting user ' + req.params.userid);
-    db.getUser(req.params.userid, user => {
-      res.send(JSON.stringify(user));
-    });
+app.get('/user/:userid/:userPassword', (req, res) => {
+  console.log('getting user ' + req.params.userid);
+  db.getUser(req.params.userid, user => {
+    let login: Login = {status : false };
+
+    if (!!user && user.login === req.params.userid &&
+      user.password === req.params.userPassword){
+        login.status=true;
+        res.send(JSON.stringify(login));
+      }
+      else{
+        login.status=false;
+          res.send(JSON.stringify(login));
+      }
+
+  });
 });
 
 app.get('/users/', (req, res) => {
-    console.log('getting users');
-    db.getUsers(user => {
-      res.send(JSON.stringify(user));
-    });
+  console.log('getting users');
+  db.getUsers(user => {
+    res.send(JSON.stringify(user));
+  });
+});
+
+app.get('/items/', (req, res) => {
+  db.getProducts(produts => {
+    res.send(JSON.stringify(produts));
+  });
 });
 
 app.listen(3000, function () {
--- bin/db.ts
+++ bin/db.ts
@@ -1,15 +1,13 @@
 import mongodb = require('mongodb');
 import assert = require('assert');
 
+import { Item } from './shared/Item';
+import { Login } from './shared/Login';
+import { User } from './shared/User';
+
 var client = mongodb.MongoClient;
 
 var url = 'mongodb://localhost:27017/products';
-
-export interface User {
-    _id: string;
-    login: string;
-    password: string;
-}
 
 export function getUser(id: string, callback: (user: User) => void) {
     client.connect(url,(err, db)=>{
@@ -32,4 +30,13 @@ export function getUsers(callback: (user: User) => void) {
         });
     });
 }
-
+export function getProducts(callback: (item: Item) => void) {
+    client.connect(url,(err, db)=>{
+        assert.equal(null,err);
+        db.collection('products').find({},{description:1, price:1, count:1, _id:0}).toArray((err, item) =>{
+            assert.equal(null, err);
+            db.close();
+            callback(item);
+        });
+    });
+}
